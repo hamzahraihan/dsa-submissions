47995c8b28feba5159173c6ecf0187541960f9ad
Hamzah Raihan <hamzahraihanif@gmail.com>
2026-04-07 11:19:13 +0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int res = 0, count = 0;
+        for(int n : nums){
+            if(n == 0){
+                count = 0;
+            }else {
+                count++;
+            }
+            res = Math.max(res, count);
+        }
+        return res;
+    }
+}
