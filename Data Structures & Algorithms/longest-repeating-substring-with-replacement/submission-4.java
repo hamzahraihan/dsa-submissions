6307419df865ed3f417c0900bf43925d12b16b9c
Hamzah Raihan <hamzahraihanif@gmail.com>
2026-04-11 08:39:49 +0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        if(s.length() == 0 || s == null) return 0;   
+
+        int l = 0, maxF = 0;
+        int res = 0;
+        int[] count = new int[26];
+        for(int r = 0; r < s.length(); r++){
+            count[s.charAt(r) - 'A']++;
+            maxF = Math.max(maxF, count[s.charAt(r) - 'A']);
+
+            while((r - l + 1) - maxF > k){
+                count[s.charAt(l) - 'A']--;
+                l++;
+            }
+
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+}
