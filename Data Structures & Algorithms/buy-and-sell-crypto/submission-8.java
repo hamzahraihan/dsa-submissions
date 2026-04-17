1391d4b2ca6932e8e6bc3aa690c60408604196c4
Hamzah Raihan <hamzahraihanif@gmail.com>
2026-04-17 19:25:12 +0700

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxP = 0;
+        int buy = prices[0];
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < buy){
+                buy = Math.min(buy, prices[i]);
+            }
+            int profit = prices[i] - buy;
+            maxP = Math.max(maxP, profit);
+        }
+        return maxP;
+    }
+}
