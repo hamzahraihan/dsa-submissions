3385aca5f773326c3ed8afb16083a5720edc1ca5
Hamzah Raihan <hamzahraihanif@gmail.com>
2026-04-20 13:18:03 +0700

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,16 @@
+// brute force O(n^2);
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(temperatures[i] < temperatures[j]){
+                    res[i] = j - i;
+                    break;
+                } 
+            }
+        }
+        return res;
+    }
+}
