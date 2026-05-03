ef8c0b832cb44840d36a8ebdb0a5904a50196cdb
Hamzah Raihan <hamzahraihanif@gmail.com>
2026-05-03 18:01:37 +0700

    Add: asteroid-collision - submission-4

---
+++ Data Structures & Algorithms/asteroid-collision/submission-4.java
@@ -0,0 +1,31 @@
+class Solution {
+    // the sign representing direction
+    // if both asteroid have the direction they wont met, otherwise they have to collide
+    // if the asteroid have bigger size than the other one, then we remove the smaller asteroid 
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        // iterate every asteroids
+        for(int a : asteroids){
+            // see if stack is not empty and compare top value in stack with asteroids i
+            while(!stack.isEmpty() && a < 0 && stack.peek() > 0){
+                if(Math.abs(a) > stack.peek()){
+                    stack.pop();
+                } else if (Math.abs(a) < stack.peek()){ // if both have the same size, than remove both asteroids
+                    a = 0;
+                } else { // if asteroid destroyed, we skip it
+                    a = 0;
+                    stack.pop();
+                }
+            }
+            // if the asteroid have value not equal to zero, then pick the asteroid to compare with another 
+            if(a != 0){
+                stack.add(a);
+            }
+        }
+        int[] res = new int[stack.size()];
+        for(int i = res.length - 1; i >= 0; i--){
+            res[i] = stack.pop();
+        }
+        return res;
+    }
+}
