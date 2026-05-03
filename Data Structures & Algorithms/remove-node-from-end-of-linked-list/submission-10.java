63a7b2b7555980b7e89d7c4485f9c3ba43837320
Hamzah Raihan <hamzahraihanif@gmail.com>
2026-05-03 09:43:32 +0700

    Add: remove-node-from-end-of-linked-list - submission-10

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-10.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int length = 0;
+        ListNode curr = head;
+        while(curr != null){
+            length++;
+            curr = curr.next;
+        }
+     
+        curr = head;
+        int target = length - n;
+        if(target == 0){
+            return head.next;
+        }
+        for(int i = 0; i < target - 1; i++){
+            curr = curr.next;
+        }
+        curr.next = curr.next.next;
+        return head;
+    }
+}
+
+// intuisi:
+/*
+cari panjang dari linked list, untuk mencari node n dari node terakhir
+misal kita mempunyai node dengan nilai [1, 2, 3, 4]
+kita mengetahui bahwa panjang dari node adalah 4 lalu kurangi dengan n 
+misal n adalah 2 maka target kita adalah 2 karena 4 - 2
+maka kita bisa menggunakan hasil tersebut sebagai target
+ */
+
+// algoritma
+/* 
+- buat variable length untuk menghitung panjang node
+- hitung panjang node menggunakan while loop
+- cari target dengan cara hitung panjang lalu kurangi n
+- jika node selanjutnya == null maka kembalikan nilai head.next 
+- buat loop untuk menggerakkan linked list berdasarkan nilai target
+- skip node target
+- kembalikan nilai head
+*/
