21d39bdd11c107fc64b302b627c67bfc64b0ae2c
Hamzah Raihan <hamzahraihanif@gmail.com>
2026-05-06 18:39:30 +0700

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.java
@@ -0,0 +1,50 @@
+class TrieNode {
+    public Map<Character, TrieNode> children;
+    public boolean isEndOfWord;
+
+    public TrieNode(){
+        this.children = new HashMap<>();
+        this.isEndOfWord = false;
+    }
+}
+
+class PrefixTree {
+    private TrieNode root;
+
+    public PrefixTree() {
+         this.root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for(char ch : word.toCharArray()){
+            if(!node.children.containsKey(ch)){
+                node.children.put(ch, new TrieNode());
+            }
+            node = node.children.get(ch);
+        }
+        node.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for(char ch : word.toCharArray()){
+            if(!node.children.containsKey(ch)){
+                return false;
+            }
+            node = node.children.get(ch);
+        }
+        return node.isEndOfWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for(char ch : prefix.toCharArray()){
+            if(!node.children.containsKey(ch)){
+                return false;
+            }
+            node = node.children.get(ch);
+        }
+        return true;
+    }
+}
