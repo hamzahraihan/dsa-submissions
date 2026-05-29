f20221302379d978f8326428a17fe3b0f03fcae7
Hamzah Raihan <hamzahraihanif@gmail.com>
2026-05-29 20:35:18 +0700

    Add: decode-ways - submission-11

---
+++ Data Structures & Algorithms/decode-ways/submission-11.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numDecodings(String s) {
+        // int[] dp = new int[s.length() + 1];
+        // dp[s.length()] = 1;
+        int dp = 0, dp2 = 0;
+        int dp1 = 1;
+        for(int i = s.length() - 1; i >= 0; i--){
+            if(s.charAt(i) == '0'){
+                dp = 0;
+            } else {
+                dp = dp1;
+                if(i + 1 < s.length() && (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7')){
+                    dp += dp2; 
+                }
+            }
+            dp2 = dp1;
+            dp1 = dp;
+            dp = dp1;
+        }
+        return dp1;
+    }
+}
