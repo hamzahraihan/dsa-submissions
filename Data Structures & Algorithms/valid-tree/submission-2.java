796703a73625b11d3fdb386b50ee12c99786f19b
Hamzah Raihan <hamzahraihanif@gmail.com>
2026-05-29 09:02:47 +0700

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] edge : edges){
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        Set<Integer> visited = new HashSet<>();
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, -1});
+        visited.add(0);
+
+        while(!q.isEmpty()){
+            int[] pair = q.poll();
+            int node = pair[0], parent = pair[1];
+            for(int nb : adj.get(node)){
+                if(nb == parent){
+                    continue;
+                }
+
+                if(visited.contains(nb)){
+                    return false;
+                }
+
+                q.offer(new int[]{nb, node});
+                visited.add(nb);
+            }
+        }
+
+        return visited.size() == n;
+    }
+}
