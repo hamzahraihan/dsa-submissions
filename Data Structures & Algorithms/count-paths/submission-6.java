d0b07d48bb67acc60b83e42a463e0e8575b77edf
Hamzah Raihan <hamzahraihanif@gmail.com>
2026-05-31 21:31:35 +0700

    Add: count-paths - submission-6

---
+++ Data Structures & Algorithms/count-paths/submission-6.java
@@ -0,0 +1,58 @@
+class Solution {
+    // recursive
+    // public int uniquePaths(int m, int n) {
+    //     return dfs(0,0,m,n);
+    // }
+
+    // private int dfs(int r, int c, int m, int n){
+    //     if(r >= (m - 1) && c >= (n - 1)) return 1;
+    //     if(r == m || c == n) return 0;
+
+    //     return dfs(r, c + 1, m, n) + dfs(r + 1, c, m,)
+    // }
+
+    /* DP: Top-Down */
+    // private int[][] memo;
+    // public int uniquePaths(int m, int n) {
+    //     memo = new int[m][n];
+    //     return dfs(0,0,m,n);
+    // }
+
+    // private int dfs(int r, int c, int m, int n){
+    //     if(r >= (m - 1) && c >= (n - 1)) return 1;
+    //     if(r == m || c == n) return 0;
+    //     if(memo[r][c] != 0) return memo[r][c];
+
+    //     return memo[r][c] = dfs(r, c + 1, m, n) + dfs(r + 1, c, m,n);
+    // }
+
+    /* DP: Bottom-Up */
+    // public int uniquePaths(int m, int n) {
+    //     int[][] dp = new int[m + 1][n + 1];
+    //     dp[m - 1][n - 1] = 1;
+
+    //     for(int r = m - 1; r >= 0; r--){
+    //         for(int c = n - 1; c >= 0; c--){
+    //             dp[r][c] += dp[r][c + 1] + dp[r + 1][c];
+    //         }
+    //     }
+
+    //     return dp[0][0];
+    // }
+    /* DP: Space-Optimized */
+    public int uniquePaths(int m, int n) {
+        int[] row = new int[n];
+        Arrays.fill(row, 1);
+
+        for(int r = 0; r < m - 1; r++){
+            int[] newRow = new int[n]; 
+            Arrays.fill(newRow, 1);
+            for(int c = n - 2; c >= 0; c--){
+                newRow[c] = newRow[c + 1] + row[c];
+            }
+            row = newRow;
+        }
+
+        return row[0];
+    }
+}
