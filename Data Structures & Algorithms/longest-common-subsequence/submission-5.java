d1582a0081c3ffa7a2bd2d4355895b432e67d7f1
Hamzah Raihan <hamzahraihanif@gmail.com>
2026-06-01 09:26:37 +0700

    Add: longest-common-subsequence - submission-5

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-5.java
@@ -0,0 +1,43 @@
+class Solution {
+    private int[][] memo;
+    public int longestCommonSubsequence(String text1, String text2) {
+        memo = new int[text1.length()][text2.length()];
+        for(int i = 0; i < text1.length(); i++){
+            for(int j = 0; j < text2.length(); j++){
+                memo[i][j] = -1;
+            }
+        }
+
+        return dfs(0, 0, text1, text2);
+    }
+
+    private int dfs(int i, int j, String text1, String text2){
+        if(i == text1.length() || j == text2.length()){
+            return 0;
+        }
+        if(memo[i][j] != -1){
+            return memo[i][j];
+        }
+        if(text1.charAt(i) == text2.charAt(j)){
+            memo[i][j] = 1 + dfs(i + 1, j + 1, text1, text2);
+        } else {
+            memo[i][j] = Math.max(dfs(i, j + 1, text1, text2), dfs(i + 1, j, text1, text2));
+        }
+
+        return memo[i][j];
+    }
+
+    // public int longestCommonSubsequence(String text1, String text2) {
+    //     int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+    //     for(int i = text1.length() - 1; i >= 0; i--){
+    //         for(int j = text2.length() - 1; j >= 0; j--){
+    //             if(text1.charAt(i) == text2.charAt(j)){
+    //                 dp[i][j] = 1 + dp[i + 1][j + 1];
+    //             } else {
+    //                 dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+    //             }
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+}
