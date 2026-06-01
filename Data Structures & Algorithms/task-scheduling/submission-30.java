ad11679680f4fb476ed6a668591099f5f937de10
Hamzah Raihan <hamzahraihanif@gmail.com>
2026-06-01 19:11:05 +0700

    Add: task-scheduling - submission-30

---
+++ Data Structures & Algorithms/task-scheduling/submission-30.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] count = new int[26];
+        for(char task : tasks){
+            count[task - 'A']++;
+        }
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0; i < 26; i++){
+            if(count[i] > 0){
+                maxHeap.offer(count[i]);
+            } 
+        }
+
+        int time = 0;
+        Queue<int[]> q = new LinkedList<>();
+        while(!maxHeap.isEmpty() || !q.isEmpty()){
+            time++;
+            if(!maxHeap.isEmpty()){
+                int cnt = maxHeap.poll() - 1;
+                if(cnt > 0){
+                    q.offer(new int[]{cnt, time + n});
+                }
+            } else {
+                time = q.peek()[1];
+            }
+
+            if(!q.isEmpty() && q.peek()[1] == time){
+                maxHeap.offer(q.poll()[0]);
+            }
+        }
+        return time; 
+    }
+}
