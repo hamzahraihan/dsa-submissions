443df3cbb0b3cbdb12cb876df4c6febdca329103
Hamzah Raihan <hamzahraihanif@gmail.com>
2026-07-04 21:12:24 +0700

    Add: find-duplicate-integer - submission-20

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-20.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast){
+                break;
+            }
+        }
+
+        int pointerA = 0, pointerB = fast;
+        while(pointerA != pointerB){
+            pointerA = nums[pointerA];
+            pointerB = nums[pointerB];
+        }
+
+        return pointerA;
+    }
+}
